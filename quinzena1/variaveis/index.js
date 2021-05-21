e77b8322374cd346a3a2b238240f979b3c97241c
unknown <bruno.labenu@gmail.com>
2021-05-20 22:49:11 -0300

    Exercício JS variáveis

---
+++ quinzena1/variaveis/index.js
@@ -0,0 +1,56 @@
+//1
+//console.log(10, 5)
+
+//2
+//console.log(10, 10, 20)
+
+//3
+// let p = trabalhoHoraDia
+//let t = recebeDia
+//let p = prompt("Quantas horas você trabalha por dia?")
+//let t = prompt("Quanto você recebe por dia?")
+//alert(`Voce recebe ${t/p} por hora`)
+
+
+//1
+// let nome
+// let idade
+// console.log(typeof nome)
+// console.log(typeof idade)
+// //D a escrita apareceu indefinida pois não foi atribuido um valor
+
+// nome = prompt("qual o seu nome?")
+// idade = prompt("qual a sua idade?")
+// console.log(typeof nome)
+// console.log(typeof idade) 
+// //F mudaram quando foi atribuido valor a elas
+
+// console.log("Olá", nome, "você tem", idade, "anos")
+
+//2
+// let pergunta1 = "você está usando roupa azul hoje?"
+// let pergunta2 = "o que você comeu hoje?"
+// let pergunta3 = "quanto você ganhou hoje?"
+
+// let resposta1 = prompt(pergunta1)
+// let resposta2 = prompt(pergunta2)
+// let resposta3 = prompt(pergunta3)
+
+// console.log(pergunta1, "-", resposta1)
+// console.log(pergunta2, "-", resposta2)
+// console.log(pergunta3, "-", resposta3)
+
+//3
+
+//EMPAQUEI AQUI NO TERCEIRO EXERCÍCIO PROFESSOR...
+
+//let a = 10
+//let b = 25
+//let a = b
+
+//console.log("O novo valor de a é", a) 
+//console.log("O novo valor de b é", b) 
+
+
+
+
