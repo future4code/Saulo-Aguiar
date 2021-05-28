2743060be167841c699a75a1730fa7920634e961
unknown <bruno.labenu@gmail.com>
2021-05-27 23:18:09 -0300

    exercicio-strings-arrays

---
+++ quinzena1/strings-arrays/index.js
@@ -0,0 +1,67 @@
+//Exercício aula 5
+
+//Exercícios de interpretação
+
+//1)
+
+//a= undefined
+//b= null
+//c= 11
+//d= 3
+//e= 3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13 
+//f= 9
+
+//2)
+//R= 27
+
+//Exercícios escrita código
+
+//1)
+
+// const nomeDoUsuario = prompt("Qual seu nome?")
+// const emailDoUsuario = prompt("Qual o seu e-mail?")
+
+// console.log("O e-mail " + emailDoUsuario + " foi cadastrado com sucesso")
+// console.log("Seja bem vindo " + nomeDoUsuario)
+
+//2)
+
+// let comida = ["hamburguer", "macarronada", "churrasco", "lamen", "pizza"] 
+
+// console.log(comida)
+
+// console.log("Essas são minhas comidas preferidas")
+// console.log("1", comida[0])
+// console.log("2", comida[1])
+// console.log("3", comida[2])
+// console.log("4", comida[3])
+// console.log("5", comida[4])
+
+// let comidaPreferida = prompt("Qual a sua comida preferida?")
+// comida[1] = comidaPreferida
+// console.log("Nova comida preferida")
+// console.log("1", comida[0])
+// console.log("2", comida[1])
+// console.log("3", comida[2])
+// console.log("4", comida[3])
+// console.log("5", comida[4])
+
+//3
+
+// let listaDeTarefas = []
+
+// let tarefa1 = prompt("Digite a primeira tarefa do dia")
+// let tarefa2 = prompt("Digite a segunda tarefa do dia")
+// let tarefa3 = prompt("Digite a terceira tarefa do dia")
+
+// listaDeTarefas.push(tarefa1) 
+// listaDeTarefas.push(tarefa2)
+// listaDeTarefas.push(tarefa3)
+
+// console.log(listaDeTarefas)
+
+// let indice = Number(prompt("Qual tarefa você já realizou? 0, 1 ou 2?"))
+
+// listaDeTarefas.splice(indice, 1)
+
+// console.log(listaDeTarefas)
